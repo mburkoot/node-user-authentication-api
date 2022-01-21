61a6848ef1e2044ca7129f669a0b9f89c89d7756
mburkoot <melannyburkoot@hotmail.com>
2022-01-21 18:01:44 -0300

    post, put e delete

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,18 @@ import userRoute from './routes/users.route';
 
 const app = express();
 
+//Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended : true }));
+
+//Configurações de Rotas
 app.use(userRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({ foo: 'Sucesso!!'});
 });
 
+//Inicialização do servidor (Qual porta)
 app.listen(3000, () => {
     console.log('Aplicação executando na porta 3000!');
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,15 +1,32 @@
 import { Router, Request, Response, NextFunction } from "express";
+import { StatusCodes } from 'http-status-codes';
 
 const userRoute = Router();
 
 userRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
     const users = [{ userName: 'Melanny' }];
-    res.status(200).send(users);
+    res.status(StatusCodes.OK).send(users);
 });
 
 userRoute.get('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
     const uuid = req.params.uuid;
-    res.status(200).send({uuid});
+    res.status(StatusCodes.OK).send({uuid});
+});
+
+userRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+   const newUser = req.body;
+   res.status(StatusCodes.CREATED).send(newUser);
+});
+
+userRoute.put('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+    const uuid = req.params.uuid;
+    const modifiedUser = req.body;
+    modifiedUser.uuid = uuid;
+    res.status(StatusCodes.OK).send({uuid});
+});
+
+userRoute.delete('/users/:uuid', (req: Request<{ uuid: string}>, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default userRoute;
