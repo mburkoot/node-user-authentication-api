50d0071ab49fe5d6f49eb4c73ea46671b4a75442
mburkoot <melannyburkoot@hotmail.com>
2022-01-21 18:29:44 -0300

    detalhes finais

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import express, { Request, Response, NextFunction} from 'express';
+import statusRoute from './routes/status.route';
 import userRoute from './routes/users.route';
 
 const app = express();
@@ -9,11 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 
 //Configurações de Rotas
+app.use(statusRoute);
 app.use(userRoute);
-
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ foo: 'Sucesso!!'});
-});
 
 //Inicialização do servidor (Qual porta)
 app.listen(3000, () => {
